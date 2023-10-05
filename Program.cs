78aa6885bae9f28710ff2d665fc153b79eeabf62
VValdo <v.sersam@gmail.com>
2023-10-05 21:13:30 +0200

    initial test

--- Program.cs
+++ Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// WebShop7
+/*
+ * 
+ *      Menu1:
+ *      1. Login --> Enter Username//Password
+ *      2. Register --> Enter Username//Email//Password --> WriteToFile!
+ *   
+ *      ----------------
+ *   
+ *      User Init Menu
+ *      1. Browse Store ----> Browse Products
+ *      2. View Cart
+ *      3. Order History
+ *      4. Logout
+ *      
+ *      ----------------
+ *      
+ *      Admin Init Menu
+ *      1. Add/Remove Listings --> WriteToFile
+ *      2. View/Edit Users
+ *      3. View Orders
+ *      4. Logout
+ *      
+ *      ----------------
+ *      
+ *      Browse Products Menu
+ *      -- Displays all products --
+ *      * Click product ---> Displays price and option "Add To Cart"
+ *      * Back
+ *      
+ *      */
+using e_commerce_project;
+
+Console.WriteLine("1.Login");
+Console.WriteLine("2.Register");
+string input = Console.ReadLine();
+if (int.TryParse(input, out int choice) && choice == 2)
+{
+    string userInput, passwordInput;
+    Register(out userInput, out passwordInput);
+    User newUser = new User(userInput, passwordInput);
+    newUser.Write();
+}
+
+// Register method
+static void Register(out string userInput, out string passwordInput)
+{
+    //while(?)
+    Console.Write("Enter username: ");
+    userInput = Console.ReadLine();
+    Console.Write("Enter password: ");
+    passwordInput = Console.ReadLine();
+}
+
+
+
+
---
+++ User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace e_commerce_project;
+    public class User
+    {
+        public string Username;
+        public string _password;
+
+        private static int _usercount = 0;
+        private readonly bool _isadmin = false;
+
+        private readonly static List<User> users = new List<User>();
+
+        // Constructor
+        public User(string username, string password)
+        {
+            Username = username;
+            _password = password;
+            _usercount++;
+        }
+        public static int UserCount => _usercount;
+
+        // Write userfile
+        public void Write()
+        {
+            users.Add(this);
+            string userDetails = Username + "//" + _password;
+            File.WriteAllText("../../../usercredentials.txt", userDetails);
+            Console.WriteLine("User Saved and Logged in");
+        }
+    }
