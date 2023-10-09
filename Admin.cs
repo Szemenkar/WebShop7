6acdf9d0928e4836c4d6a0e65b21cfcb1491f628
VValdo <v.sersam@gmail.com>
2023-10-09 11:49:08 +0200

    09oktober

---
+++ Admin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Admin class
+
+//    Lägg till och ta bort produkter
+//    Visa och redigera alla kunders information
+//    Översikt över alla beställningar och transaktioner
+
+
+namespace e_commerce_project;
+public class Admin
+{
+    
+}
--- Program.cs
+++ Program.cs
@@ -1,57 +1,19 @@
-﻿// WebShop7
-/*
- * 
- *      Menu1:
- *      1. Login --> Enter Username//Password
- *      2. Register --> Enter Username//Email//Password --> WriteToFile!
- *   
- *      ----------------
- *   
- *      User Init Menu
- *      1. Browse Store ----> Browse Products
- *      2. View Cart
- *      3. Order History
- *      4. Logout
- *      
- *      ----------------
- *      
- *      Admin Init Menu
- *      1. Add/Remove Listings --> WriteToFile
- *      2. View/Edit Users
- *      3. View Orders
- *      4. Logout
- *      
- *      ----------------
- *      
- *      Browse Products Menu
- *      -- Displays all products --
- *      * Click product ---> Displays price and option "Add To Cart"
- *      * Back
- *      
- *      */
-using e_commerce_project;
-
-Console.WriteLine("1.Login");
-Console.WriteLine("2.Register");
-string input = Console.ReadLine();
-if (int.TryParse(input, out int choice) && choice == 2)
+﻿using e_commerce_project;
+// Apparel 7
+string[] title = File.ReadAllLines("../../../title.txt");
+foreach (var line in title)
 {
-    string userInput, passwordInput;
-    Register(out userInput, out passwordInput);
-    User newUser = new User(userInput, passwordInput);
-    newUser.Write();
+    Console.WriteLine(line);
 }
 
-// Register method
-static void Register(out string userInput, out string passwordInput)
-{
-    //while(?)
-    Console.Write("Enter username: ");
-    userInput = Console.ReadLine();
-    Console.Write("Enter password: ");
-    passwordInput = Console.ReadLine();
-}
+// "../../../"
 
+// Användarinfo -- customerLogin.txt 
+// Admininfo -- adminLogin.txt
+// Produktinfo -- products.txt
+// Kvitton -- receipts.txt
 
+// Add to Cart
+// Complete Purchase --> makeOrder (save receipt)
 
 
