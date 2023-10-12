44990d12327af41b78a7f46e1bdfa1c7f6781280
VValdo <v.sersam@gmail.com>
2023-10-12 10:16:29 +0200

    tiny changes

--- Program.cs
+++ Program.cs
@@ -1,13 +1,9 @@
 // Apparel 7
-// "../../../"
 
 // Användarinfo -- /data/customerLogin.txt 
 // Admininfo -- /data/adminLogin.txt
 // Produktinfo -- /data/products.txt
 // Kvitton -- /data/receipts.txt
-
-// Add to Cart
-// Complete Purchase --> makeOrder (save receipt)
 
 string[] title = File.ReadAllLines("../../../data/title.txt");
 foreach (var line in title)
